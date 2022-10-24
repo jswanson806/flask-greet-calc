36521df75088c8574b4666c2e0325a822654c423
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2022-10-24 04:58:26 -0500

    finished exercise

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,24 @@
 # Put your app in here.
+from flask import Flask, request
+import operations
+app = Flask(__name__)
+
+@app.route('/add')
+def add():
+    """Add a and b"""
+    return str(operations.add(int(request.args['a']), int(request.args['b'])))
+
+@app.route('/sub')
+def sub():
+    """Subtract a and b"""
+    return str(operations.sub(int(request.args['a']), int(request.args['b'])))
+
+@app.route('/mult')
+def mult():
+    """Multiply a and b"""
+    return str(operations.mult(int(request.args['a']), int(request.args['b'])))
+
+@app.route('/div')
+def div():
+    """Divide a by b"""
+    return str(operations.div(int(request.args['a']), int(request.args['b'])))
